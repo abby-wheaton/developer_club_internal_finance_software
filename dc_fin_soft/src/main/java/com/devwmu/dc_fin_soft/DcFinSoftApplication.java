94499e8c9a2f484df32cf44e5ca073895d49dfbc
Wheatthin25 <abigail.l.wheaton@wmich.edu>
2026-07-17 10:56:18 -0400

    adding api docs

--- dc_fin_soft/src/main/java/com/devwmu/dc_fin_soft/DcFinSoftApplication.java
+++ dc_fin_soft/src/main/java/com/devwmu/dc_fin_soft/DcFinSoftApplication.java
@@ -8,17 +8,19 @@ import io.swagger.v3.oas.annotations.info.Contact;
 
 @SpringBootApplication
 @OpenAPIDefinition(
-	info = @Info(
-		title = "WMU Developer Club's Financial API",
-		version = "v1.0",
-		description = "What this api is",
-		contact = @Contact(name = "API support", email = "devclubidk@email.com")
-	)
+    info = @Info(
+        title = "WMU Developer Club's Financial API",
+        version = "v1.0",
+        description = "An API to interact with the backend of WMU Developer Club's database to manage finances and budgeting.",
+        contact = @Contact(name = "API Support Technician", email = "bluewheatthin@gmail.com")
+    )
 )
 public class DcFinSoftApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DcFinSoftApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DcFinSoftApplication.class, args);
+    }
 
 }
+
+
